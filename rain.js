9109a1d0de63c101915fbe3bee3581621d23eb0a
Manasa1010 <64597788+Manasa1010@users.noreply.github.com>
2020-06-15 22:56:30 +0530

    Add files via upload

--- rain.js
+++ rain.js
@@ -9,7 +9,7 @@ class Rain{
       this.body=Bodies.rectangle(x,y,5,30,options)
       this.width=5;
       this.height=30;
-      this.t = []
+     
 
       World.add(world,this.body)
   }
@@ -17,7 +17,7 @@ class Rain{
       var pos=this.body.position;
       var angle=this.body.angle;
 
-      if(this.body.velocity.x<10 && this.body.position.x>200){
+     /* if(this.body.velocity.x<10 && this.body.position.x>200){
         var pos1 = [this.body.position.x,this.body.position.y]
         this.t.push(pos);
         for(var i=0;i=this.t.length;i=i+1){
@@ -25,15 +25,15 @@ class Rain{
           speed=random(50,90);
           rect.velocityY=speed;
         }
-      }
+      }*/
 
-   /* push();
+    push();
     translate(pos.x,pos.y);
     rotate(angle);
     fill(225);
     //rectMode(CENTER);
     rect(0,0,this.width,this.height);
-    pop();*/
+    pop();
     
   }
 }
--- sketch.js
+++ sketch.js
@@ -2,25 +2,29 @@ const Engine=Matter.Engine;
 const World=Matter.World;
 const Bodies=Matter.Bodies;
 var engine,world;
-var speed;
+t = []
 
 function setup() {
   createCanvas(800,400);
   engine = Engine.create();
   world = engine.world;
   //createSprite(400, 200, 50, 50);
+  speed=random(50,90)
+ for(k=0;k<=width ; k=k+80){
+ t.push(new Rain(20,speed=random(50,90)));
+ }
+
   
- // World.add(world,rect1);
-
-
-  //speed=random(50,90)
  // drop = new Rain(200,0);
  // drop.velocityY=speed;
 }
 
 function draw() {
   background(0);  
-  Engine.update(engine)
+  Engine.update(engine) 
+  for(k=0 ; k<=width; k++){
+    t[k].display();
+  }
  //drop.display();
   drawSprites();
 }
